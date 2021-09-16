6c7a0607a92359f59942959b8d6b1383f2fbbee3
robson <robsonklima@gmail.com>
2021-09-15 23:26:04 -0300

    Tasks e Task

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Tasks from './components/Tasks';
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: '1',
+      title: 'Task 1',
+      completed: false
+    },
+    {
+      id: '2',
+      title: 'Task 2',
+      completed: false
+    }
+  ]);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <Tasks tasks={tasks} />
+      </div>
+    </>
   );
 }
 
